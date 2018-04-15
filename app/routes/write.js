8613d623e8f9f2bc5fc66a309b19828c4651cc65
Suzanne Knop <sknop8@gmail.com>
2018-04-15 18:00:50 -0400

    Adding some more writing and style edits

--- app/routes/write.js
+++ app/routes/write.js
@@ -12,5 +12,18 @@ router.get('/on-goal-orientation', function(req, res, next) {
   res.render('write/write_goal_orientation');
 });
 
+router.get('/twice', function(req, res, next) {
+  res.render('write/twice');
+});
+
+router.get('/the-driver', function(req, res, next) {
+  res.render('write/the_driver');
+});
+
+router.get('/the-fence', function(req, res, next) {
+  res.render('write/the_fence');
+});
+
+
 
 module.exports = router;
