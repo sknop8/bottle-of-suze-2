ddc91972186da2b09e749b5822b05231e1f4a57a
Suzanne Knop <sknop8@gmail.com>
2018-05-07 05:33:02 -0400

    yea

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'bottle of suze' });
 });
 
+router.get('/suzanne-channel', function(req, res, next) {
+  res.render('suzanne');
+});
+
 module.exports = router;
