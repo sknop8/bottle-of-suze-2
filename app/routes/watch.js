83482f560390deec745e7d6423b9f4f71267acc3
Suzanne Knop <sknop8@gmail.com>
2020-03-29 15:07:57 -0400

    Adds watch menu by year.

--- app/routes/watch.js
+++ app/routes/watch.js
@@ -3,8 +3,32 @@ var router = express.Router();
 
 /* GET watch listing. */
 router.get('/', function(req, res, next) {
-  res.render('watch/watch', {
-    title: 'watch | bottle of suze'
+  res.render('watch/2020', {
+    title: 'watch | 2020 | bottle of suze'
+  });
+});
+
+router.get('/2020', function(req, res, next) {
+  res.render('watch/2020', {
+    title: 'watch | 2020 | bottle of suze'
+  });
+});
+
+router.get('/2019', function(req, res, next) {
+  res.render('watch/2019', {
+    title: 'watch | 2019 | bottle of suze'
+  });
+});
+
+router.get('/2018', function(req, res, next) {
+  res.render('watch/2018', {
+    title: 'watch | 2018 | bottle of suze'
+  });
+});
+
+router.get('/2017', function(req, res, next) {
+  res.render('watch/2017', {
+    title: 'watch | 2017 | bottle of suze'
   });
 });
 
